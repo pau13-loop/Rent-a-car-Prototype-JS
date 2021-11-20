8378fd3a6b2d5a250f18dcbf8926d7aa317b8fde
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-20 17:42:34 +0100

    feat: add json data strcture and mock with data

---
+++ src/data/mockCars.js
@@ -0,0 +1,163 @@
+export const mockCarsDB = [
+    // RENAULT 
+    {
+        model: "Clio Sport",
+        brand: "renault",
+        category: "premium", 
+        type: "diesel",
+        color: "yellow",
+        asientos: 5,
+        year: 2001,
+        price: 20, 
+        avaialble: true,
+        characteristics: ["tinted crystals", "turbo", "sportive", "compact"]
+    },
+    {
+        model: "Renault 4",
+        brand: "renault",
+        category: "classic", 
+        type: "gasoline",
+        color: "red",
+        asientos: 5,
+        year: 1992,
+        price: 30, 
+        avaialble: true,
+        characteristics: ["compact", "low consumption", "radio cassette"]
+    },
+    {
+        model: "Kangoo",
+        brand: "renault",
+        category: "van", 
+        type: "diesel",
+        color: "yellow",
+        asientos: 5,
+        year: 2008,
+        price:15, 
+        avaialble: true,
+        characteristics: ["large", "big trunk", "tall", "familiar"]
+    },
+    // SEAT
+    {
+        model: "Seat Ibiza",
+        brand: "seat",
+        category: "common", 
+        type: "diesel",
+        color: "white",
+        asientos: 5,
+        year: 2015,
+        price: 13, 
+        avaialble: true,
+        characteristics: ["low consumption", "familliar"]
+    },
+    {
+        model: "Seat Leaon FR",
+        brand: "seat",
+        category: "premium", 
+        type: "hybrid",
+        color: "black",
+        asientos: 5,
+        year: 2017,
+        price: 18, 
+        avaialble: true,
+        characteristics: ["sportive", "low consumption", "heated seats", "navigation assistant"]
+    },
+    {
+        model: "Cordoba",
+        brand: "seat",
+        category: "common", 
+        type: "electric",
+        color: "blue",
+        asientos: 5,
+        year: 2019,
+        price: 16, 
+        avaialble: true,
+        characteristics: ["low consumption", "sunroof", "familiar"]
+    },
+    // VOLKSWAGEN
+    {
+        model: "Bettle",
+        brand: "volkswagen",
+        category: "classic", 
+        type: "gasoline",
+        color: "blue",
+        asientos: 2,
+        year: 1990,
+        price: 30, 
+        avaialble: true,
+        characteristics: ["convertible", "radio cassete", "compact"]
+    },
+    {
+        model: "Golf",
+        brand: "volkswagen",
+        category: "common", 
+        type: "diesel",
+        color: "white",
+        asientos: 5,
+        year: 2018,
+        price: 16, 
+        avaialble: true,
+        characteristics: ["compact", "navigation assistant", "sunroof"]
+    },
+    {
+        model: "Sirocco",
+        brand: "volkswagen",
+        category: "premium", 
+        type: "hybrid",
+        color: "white",
+        asientos: 4,
+        year: 2021,
+        price: 27, 
+        avaialble: true,
+        characteristics: ["sportive", "tinted crystals", "heated seats", "navigations assistant", "heated seats", "sunroof"]
+    },
+    {
+        model: "California",
+        brand: "vollkswagen",
+        category: "van", 
+        type: "diesel",
+        color: "white",
+        asientos: 6,
+        year: 2016,
+        price: 35, 
+        avaialble: true,
+        characteristics: ["tall", "large", "navigation assistant", "familiar", "accomodation"]
+    },
+    // FORD
+    {
+        model: "Focus",
+        brand: "ford",
+        category: "common", 
+        type: "hybrid",
+        color: "black",
+        asientos: 5,
+        year: 2017,
+        price: 14, 
+        avaialble: true,
+        characteristics: ["low consumption", "familiar", "tinted crystals"]
+    },
+    {
+        model: "C-Max",
+        brand: "ford",
+        category: "common", 
+        type: "diesel",
+        color: "grey",
+        asientos: 5,
+        year: 2015,
+        price: 14, 
+        avaialble: true,
+        characteristics: ["familiar", "big trunk", "large", "sunroof"]
+    },
+    {
+        model: "Galaxy",
+        brand: "ford",
+        category: "van", 
+        type: "gasoline",
+        color: "black",
+        asientos: 7,
+        year: 2012,
+        price: 18, 
+        avaialble: true,
+        characteristics: ["familiar", "sunroof", "big trink", "large"]
+    }
+]
+
